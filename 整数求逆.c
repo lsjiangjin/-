94b5af9662bead198c5e0f83648b5b874f8ff8f5
lsjiangjin <57002414+lsjiangjin@users.noreply.github.com>
2019-10-27 12:42:18 +0800

    第一次创建整数求逆
    通过C语言完成对一个三位正整数的整数求逆

---
+++ 整数求逆.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void)
+{
+	float a;
+	int c, d, f;
+	printf_s("请输入三位整数：");
+	scanf_s("%f", &a);
+	if (a<0)
+	{
+		printf_s("error");
+	}
+	else
+	{
+		if (a / 100 > 0 && a / 1000 == 0 &&(int)a==a)
+		{
+			c = (int)a / 100;
+			d = (int)(a - c * 100) / 10;
+			f = a - c * 100 - d * 10;
+			printf_s("%d", f * 100 + d * 10 + c);
+		}
+		else
+		{
+			printf_s("error");
+		}
+	}
+}
